8824939570413e9d03d6746c4b82a249ac38df16
elena_kau <lelickau@yandex.ru>
2022-05-22 15:11:31 +0200

    -

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,19 @@ const App: FC = () => {
 	return (
 		<>
 			<h3 className='title'>Модальное окно</h3>
-	<a target="_blank" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/modal">GitHub Code</a>
+	<a target="_blank" rel="noopener noreferrer" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/modal">GitHub Code</a>
 	<Content />
 	<div className="line"></div>
 	<h3 className='title'>Drag and drop</h3>
-	<a target="_blank" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/dragEndDrop">GitHub Code</a>
+	<a target="_blank" rel="noopener noreferrer" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/dragEndDrop">GitHub Code</a>
 	<DragNDrop />
 	<div className="line"></div>
 		<h3 className='title'>Аккордеон</h3>
-		<a target="_blank" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/accordion">GitHub Code</a>
+		<a target="_blank" rel="noopener noreferrer" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/accordion">GitHub Code</a>
 	<AccordionApp />
 	<div className="line"></div>
 	<h3 className='title'>Динамическая пагинация</h3>
-	<a target="_blank" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/scrollPagination">GitHub Code</a>
+	<a target="_blank" rel="noopener noreferrer" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/scrollPagination">GitHub Code</a>
 	<ScrollPagination />
 		</>
 	)

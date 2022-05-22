e7dec9310e27b275e920418b21504a144c13990b
elena_kau <lelickau@yandex.ru>
2022-05-22 15:02:33 +0200

    update style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react'
-import { Routes, Route } from "react-router-dom"
 import AccordionApp from './components/accordion/AccordionApp'
 import DragNDrop from './components/dragEndDrop/DragNDrop'
 import Content from './components/modal/Content'
@@ -7,14 +6,23 @@ import ScrollPagination from './components/scrollPagination/ScrollPagination'
 
 const App: FC = () => {
 	return (
-		<div>
-		<Routes>
-            <Route path="/modal" element={<Content />} />
-            <Route path="/drop" element={<DragNDrop />} />
-            <Route path="/accordion" element={<AccordionApp />} />
-            <Route path="/pagination-scroll" element={<ScrollPagination />} />
-        </Routes>
-		</div>
+		<>
+			<h3 className='title'>Модальное окно</h3>
+	<a target="_blank" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/modal">GitHub Code</a>
+	<Content />
+	<div className="line"></div>
+	<h3 className='title'>Drag and drop</h3>
+	<a target="_blank" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/dragEndDrop">GitHub Code</a>
+	<DragNDrop />
+	<div className="line"></div>
+		<h3 className='title'>Аккордеон</h3>
+		<a target="_blank" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/accordion">GitHub Code</a>
+	<AccordionApp />
+	<div className="line"></div>
+	<h3 className='title'>Динамическая пагинация</h3>
+	<a target="_blank" className="link__git" href="https://github.com/lelickau/component__collection/tree/main/react/scrollPagination">GitHub Code</a>
+	<ScrollPagination />
+		<>
 	)
 }
 
